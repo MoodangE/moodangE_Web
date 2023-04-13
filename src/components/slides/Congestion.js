9cac441154e59aed3ca488ab2cb5b2e1d4126d37
Chung Hyunwoo <woo980711@naver.com>
2023-04-13 09:42:00 +0900

    [03/28] dev: congestion levels color

--- src/components/slides/Congestion.js
+++ src/components/slides/Congestion.js
@@ -2,14 +2,24 @@ import React from "react";
 import "./Congestion.css"
 
 function Congestion({place_congestion, level}) {
+    const congestion_level = [
+        {'en': 'Spare', 'ko': '여유'},
+        {'en': 'General', 'ko': '보통'},
+        {'en': 'Caution', 'ko': '주의'},
+        {'en': 'Congestion', 'ko': '혼잡'}
+    ];
     return (
         <div className="CongestionWide">
             <span className="PlaceTitle">
                 {place_congestion} 혼잡도:
             </span>
-            <span className="LevelTitle">
-                {level}
-            </span>
+            {congestion_level
+                .filter((levels) => (levels.en === level))
+                .map((levels) => (
+                    <span key={levels.en} className={`LevelTitle ${levels.en}`}>
+                        {levels.ko}
+                    </span>
+                ))}
         </div>
     );
 }
