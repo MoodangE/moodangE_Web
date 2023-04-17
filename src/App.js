8ae78ad8cb11651987e9345c87a4c556250dd8ea
LeeYeonhee-00 <98442485+LeeYeonhee-00@users.noreply.github.com>
2023-04-17 17:13:03 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import Header from "./components/HeaderPage"
-import Slide from "./components/SlideListPage"
-import {useEffect, useState} from "react";
-import {BrowserView, MobileView} from "react-device-detect";
+import Header from "./components/HeaderPage";
+import Slide from "./components/SlideListPage";
+import { useEffect, useState } from "react";
+import { BrowserView, MobileView } from "react-device-detect";
 import Modal from "./components/modal/Modal";
 
 function App() {
-    // 모바일 100vh 스크롤 생기는 문제 해결
-    function setScreenSize() {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty("--vh", `${vh}px`); //"--vh"라는 속성으로 정의해준다.
-    }
+  // 모바일 100vh 스크롤 생기는 문제 해결
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`); //"--vh"라는 속성으로 정의해준다.
+  }
 
-    useEffect(() => {
-        setScreenSize();
-    });
+  useEffect(() => {
+    setScreenSize();
+  });
 
-    const browserStyle = {
-        height: '100vh',
-        width: '100vm',
-        placeItems: 'center',
-        display: 'grid'
-    }
-    const browserTextStyle = {
-        fontFamily: '\'JejuGothic\', cursive',
-        fontSize: '3vh'
-    }
+  const browserStyle = {
+    height: "100vh",
+    width: "100vm",
+    placeItems: "center",
+    display: "grid",
+  };
+  const browserTextStyle = {
+    fontFamily: "'JejuGothic', cursive",
+    fontSize: "3vh",
+  };
 
-    const [modalOpen, setModalOpen] = useState(false)
-    const modalShow = () => {
-        setModalOpen(true);
-        document.body.style.overflow = "hidden";
-    };
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalShow = () => {
+    setModalOpen(true);
+    document.body.style.overflow = "hidden";
+  };
 
-    return (
-        <div className="App">
-            <BrowserView style={browserStyle}>
+  return (
+    <div className="App">
+      {/* <BrowserView style={browserStyle}>
                 <div style={browserTextStyle}>Please connect to mobile</div>
             </BrowserView>
-            <MobileView>
-            {modalOpen && <Modal setModalOpen={setModalOpen}/>}
-            <Header modalShow={modalShow}/>
-            <Slide/>
-            </MobileView>
-        </div>
-    );
+            <MobileView> */}
+      {modalOpen && <Modal setModalOpen={setModalOpen} />}
+      <Header modalShow={modalShow} />
+      <Slide />
+      {/* </MobileView> */}
+    </div>
+  );
 }
 
 export default App;
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import "./Modal.css"
+import React from "react";
+import "./Modal.css";
+import Vector from "../../static/img/Vector.png";
 
-function Modal({setModalOpen}) {
-    // 모달 끄기
-    const closeModal = () => {
-        setModalOpen(false);
-        document.body.style.overflow = "unset";
-    };
+function Modal({ setModalOpen }) {
+  // 모달 끄기
+  const closeModal = () => {
+    setModalOpen(false);
+    document.body.style.overflow = "unset";
+  };
 
-    return (
-        // modal_ground 즉 modal 뒤 배경을 누르면 닫히는 이벤트
-        <div className="modal_ground" onClick={closeModal}>
-            {/*(e) => e.stopPropagation() 는 modal 창은 눌러도 닫히지 않도록 modal_ground action 을 제한*/}
-            <div className="modal_container"
-                 onClick={(e) => e.stopPropagation()}>
-                <button className="close" onClick={closeModal}>
-                    닫기
-                </button>
-                <div className="modal_title">
-                    가천대 무당이 알리미
-                </div>
-            </div>
+  return (
+    // modal_ground 즉 modal 뒤 배경을 누르면 닫히는 이벤트
+    <div className="modal_ground" onClick={closeModal}>
+      {/*(e) => e.stopPropagation() 는 modal 창은 눌러도 닫히지 않도록 modal_ground action 을 제한*/}
+      <div className="modal_container" onClick={(e) => e.stopPropagation()}>
+        <div className="modal_title">가천대 무당이 알리미</div>
+        <div>
+          <div className="info_title">운행정보</div>
+          <div className="vector1">
+            <img src={Vector} />
+          </div>
         </div>
-    );
+
+        <button className="close" onClick={closeModal}>
+          닫기
+        </button>
+
+        <div className="vector2">
+          <img src={Vector} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Modal
+export default Modal;
